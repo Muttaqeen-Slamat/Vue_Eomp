8196cfb0608fe010044c787927ecfd51081d4cc9
Muttaqeen-Slamat <muttaqeens@gmail.com>
2024-01-08 11:43:07 +0200

    footer added to whole website

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <p class="lead">
+                Muttaqeen's Portfolio &copy;
+                <span>{{ currYear }}</span>
+            </p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp',
+        data(){
+            return{
+                currYear: new Date().getFullYear()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
